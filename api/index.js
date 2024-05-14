72b9eac05bc0cd9dc695b07eb1d7c65944fb3da4
mylkner <mylofaulkner27@gmail.com>
2024-05-14 06:52:29 +0200

    errorHandler fn and middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRouter from "./routes/authRoute.js";
 
 dotenv.config();
 
@@ -15,3 +16,16 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth/", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
