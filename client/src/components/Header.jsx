f8d01dd0459896db3dffac0fdf5d89d576e531d6
mylkner <mylofaulkner27@gmail.com>
2024-05-14 05:30:20 +0200

    added conditional sign-in or profile links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,21 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+    const { currentUser } = useSelector((state) => state.user);
+
     return (
         <header className="bg-blue-900 shadow-md">
             <div className="max-w-[85%] flex items-center justify-between mx-auto text-white p-4 text-xl">
                 <Link to={"/"}>Play Sudoku</Link>
-                <div className="flex gap-2">
+                <div className="flex gap-3">
                     <Link to={"/:user/history"}>History</Link>
-                    <Link to={"/profile"}>Profile</Link>
+
+                    {currentUser ? (
+                        <Link to={"/profile"}>Profile</Link>
+                    ) : (
+                        <Link to={"/sign-in"}>Sign-In</Link>
+                    )}
                 </div>
             </div>
         </header>
