1e384b5a598143c9c17fa576ba342e36418f8ad3
mylkner <mylofaulkner27@gmail.com>
2024-05-16 05:58:01 +0200

    added disabled prop

--- client/src/components/grid/GridCell.jsx
+++ client/src/components/grid/GridCell.jsx
@@ -3,9 +3,10 @@ const GridCell = (props) => {
         <input
             type="number"
             value={props.value}
-            className={`${props.bg} ${props.border} text-2xl text-center w-[11.1111111%] focus:outline-none caret-transparent cursor-pointer h-[50px]`}
-            onFocus={props.onFocus}
+            className={`${props.bg} border ${props.border} text-2xl text-center w-[11.1111111%] focus:outline-none caret-transparent cursor-pointer h-[50px]`}
+            onClick={props.onClick}
             onChange={props.onChange}
+            disabled={props.disabled}
         />
     );
 };
