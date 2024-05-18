d9c383085c5098ebf38f4cb675b9e6f223317120
mylkner <mylofaulkner27@gmail.com>
2024-05-18 11:06:19 +0200

    changed width

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <header className="bg-blue-900 shadow-md">
-            <div className="max-w-[85%] flex items-center justify-between mx-auto text-white p-4 text-xl">
+            <div className="max-w-[80%] flex items-center justify-between mx-auto text-white p-4 text-xl">
                 <Link to={"/"}>Play Sudoku</Link>
                 <div className="flex gap-3">
                     {currentUser ? (
