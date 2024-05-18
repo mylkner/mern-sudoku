a431745b318983701f8f4e1f0c7740f8d3168b0e
mylkner <mylofaulkner27@gmail.com>
2024-05-18 04:21:21 +0200

    added size prop to spinner

--- client/src/components/signinup/SubmitButton.jsx
+++ client/src/components/signinup/SubmitButton.jsx
@@ -10,7 +10,7 @@ const SubmitButton = ({ loading, text, error }) => {
                 disabled={loading}
                 className="disabled:opacity-60 flex justify-center items-center w-full rounded-lg bg-blue-500 border border-black text-white text-2xl p-3 mt-3"
             >
-                {loading ? <Spinner /> : text}
+                {loading ? <Spinner size="text-2xl" /> : text}
             </button>
         </>
     );
