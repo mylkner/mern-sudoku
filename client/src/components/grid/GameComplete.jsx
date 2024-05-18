1d24f3be7f3c1c9e34580fb0b6d91341c757cc67
mylkner <mylofaulkner27@gmail.com>
2024-05-18 04:27:30 +0200

    addded spinner if loading

--- client/src/components/grid/GameComplete.jsx
+++ client/src/components/grid/GameComplete.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setGameComplete } from "../../redux/sudokuSlice";
 import axios from "axios";
+import FullScreenSpinner from "../FullScreenSpinner";
 
 const GameComplete = ({ board, onClick, mistakes }) => {
     const dispatch = useDispatch();
@@ -46,32 +47,30 @@ const GameComplete = ({ board, onClick, mistakes }) => {
         }
     };
 
-    return (
-        gameComplete &&
-        !loading && (
-            <div className="fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-70 w-full h-full flex items-center justify-center">
-                <div className="bg-white rounded-lg border border-black max-w-[80%] p-5 text-xl">
-                    Good job! You have completed this sudoku board with{" "}
-                    {mistakes} mistake(s).
-                    <br />
-                    <br />
-                    Your time was:{" "}
-                    {mins + " minutes and " + seconds + " seconds"}
-                    <br />
-                    <br />
-                    Your difficulty was:{" "}
-                    {difficulty.slice(0, 1).toUpperCase() + difficulty.slice(1)}
-                    <br />
-                    <br />
-                    <button
-                        className="w-full bg-blue-500 p-3 text-white font-bold text-xl border border-black rounded-lg"
-                        onClick={onClick}
-                    >
-                        Continue
-                    </button>
-                </div>
+    return gameComplete && !loading ? (
+        <div className="fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-70 w-full h-full flex items-center justify-center">
+            <div className="bg-white rounded-lg border border-black max-w-[80%] p-5 text-xl">
+                Good job! You have completed this sudoku board with {mistakes}{" "}
+                mistake(s).
+                <br />
+                <br />
+                Your time was: {mins + " minutes and " + seconds + " seconds"}
+                <br />
+                <br />
+                Your difficulty was:{" "}
+                {difficulty.slice(0, 1).toUpperCase() + difficulty.slice(1)}
+                <br />
+                <br />
+                <button
+                    className="w-full bg-blue-500 p-3 text-white font-bold text-xl border border-black rounded-lg"
+                    onClick={onClick}
+                >
+                    Continue
+                </button>
             </div>
-        )
+        </div>
+    ) : (
+        loading && <FullScreenSpinner />
     );
 };
 
