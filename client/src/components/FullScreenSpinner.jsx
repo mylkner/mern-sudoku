6426cd8d14c3dfdd8c1c66357418942fc8e1820f
mylkner <mylofaulkner27@gmail.com>
2024-05-19 15:43:51 +0200

    adjusted opacity

--- client/src/components/FullScreenSpinner.jsx
+++ client/src/components/FullScreenSpinner.jsx
@@ -2,7 +2,7 @@ import Spinner from "./Spinner";
 
 const FullScreenSpinner = () => {
     return (
-        <div className="fixed top-0 right-0 bottom-0 left-0 bg-opacity-60 bg-black flex items-center justify-center overflow-hidden z-50">
+        <div className="fixed top-0 right-0 bottom-0 left-0 bg-opacity-20 bg-black flex items-center justify-center overflow-hidden z-50">
             <Spinner size={"text-5xl"} />
         </div>
     );
