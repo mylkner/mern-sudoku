5bfb59c2161b45cc7adec3dbb80b0ad4af032bc7
mylkner <mylofaulkner27@gmail.com>
2024-05-25 21:27:16 +0200

    domain name but like, for real for real

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -63,7 +63,7 @@ export const signin = async (req, res, next) => {
         res.cookie("accessToken", token, {
             httpOnly: false,
             secure: true,
-            domain: "sudoku-theta-flax.vercel.app",
+            domain: ".sudoku-theta-flax.vercel.app/",
             path: "/",
         })
             .status(200)
