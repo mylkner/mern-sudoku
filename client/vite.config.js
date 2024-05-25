20c345b99415096268d5195aae779ae5b0e82186
mylkner <mylofaulkner27@gmail.com>
2024-05-25 18:30:44 +0200

    server url

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,10 +10,7 @@ export default defineConfig(({ mode }) => {
         server: {
             proxy: {
                 "/api": {
-                    target:
-                        mode === "production"
-                            ? env.VITE_SERVER_URL
-                            : "http://localhost:3000",
+                    target: "https://mern-sudoku.vercel.app",
                     changeOrigin: true,
                 },
             },
