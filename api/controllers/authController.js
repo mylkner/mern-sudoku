987ad063ab999a998077883e5238bcd2b6ff2dfd
mylkner <mylofaulkner27@gmail.com>
2024-05-25 19:55:14 +0200

    set cookie to https instead of http

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -60,10 +60,12 @@ export const signin = async (req, res, next) => {
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
 
-        res.cookie("accessToken", token, { httpOnly: true }).status(200).json({
-            success: true,
-            user: rest,
-        });
+        res.cookie("accessToken", token, { httpOnly: false, secure: true })
+            .status(200)
+            .json({
+                success: true,
+                user: rest,
+            });
     } catch (error) {
         next(error);
     }
