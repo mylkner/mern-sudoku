47ceb9bca5dfd798d606905f4f6408f365f48335
mylkner <mylofaulkner27@gmail.com>
2024-05-25 17:27:51 +0200

    added env vars

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        proxy: {
-            "/api": {
-                target: import.meta.env.VITE_SERVER,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+
+    return {
+        plugins: [react()],
+        server: {
+            proxy: {
+                "/api": {
+                    target: process.env.VITE_SERVER_URL,
+                },
             },
         },
-    },
+        define: {
+            "process.env": JSON.stringify(env),
+        },
+    };
 });
