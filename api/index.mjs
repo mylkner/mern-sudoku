08d4ea38d3dde4e42362250488c66df436ea00d1
mylkner <mylofaulkner27@gmail.com>
2024-05-26 17:39:57 +0200

    disable caching

--- api/index.mjs
+++ api/index.mjs
@@ -26,8 +26,14 @@ app.get("/", (req, res) => {
     res.status(200).json("server is running");
 });
 
-app.use("/api/auth/", authRouter);
 app.use("/api/sudoku/", sudokoRouter);
+
+app.use((req, res, next) => {
+    res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
+    next();
+});
+
+app.use("/api/auth/", authRouter);
 app.use("/api/user/", userRouter);
 
 app.use((err, req, res, next) => {
